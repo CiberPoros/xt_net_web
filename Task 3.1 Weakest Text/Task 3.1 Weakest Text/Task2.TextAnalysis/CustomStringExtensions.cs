5fed8df2cdb30ddb0ed181228df8ac3b0682b092
Завенягин Максим <maximzombi@yandex.ru>
2020-06-30 17:40:35 +0400

    Task 3.1.2: base functional added

---
+++ Task 3.1 Weakest Text/Task 3.1 Weakest Text/Task2.TextAnalysis/CustomStringExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using CString;
+
+namespace Task2.TextAnalysis
+{
+    public static class CustomStringExtensions
+    {
+        public static CustomString FormatCenter(this CustomString str, int symbolsCount)
+        {
+            int offset = Math.Max(symbolsCount - str.Length, 0);
+
+            char[] res = new char[Math.Max(str.Length, symbolsCount)];
+
+            for (int i = 0; i < offset / 2; i++)
+                res[i] = ' ';
+
+            for (int i = 0; i < str.Length; i++)
+                res[offset / 2 + i] = str[i];
+
+            for (int i = offset / 2 + str.Length; i < res.Length; i++)
+                res[i] = ' ';
+
+            return CustomString.CreateInstance(res);
+        }
+    }
+}
---
+++ Task 3.1 Weakest Text/Task 3.1 Weakest Text/Task2.TextAnalysis/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CString;
+
+namespace Task2.TextAnalysis
+{
+    class Program
+    {
+        private static readonly char[] separators = new char[] { ' ', ',', '.', '!', ';', ';', '?', '(', ')', '-', '_', '\'', '"', '@' };
+
+        static void Main()
+        {
+            Console.WriteLine("Введите текст...");
+
+            var text = CustomString.CreateInstance(Console.ReadLine().ToCharArray());
+
+            var frequencies = CalculateStringsFrequencies(text, false, separators);
+            var wordsCount = frequencies.Values.Sum();
+
+            Console.WriteLine($"Всего слов: {wordsCount}");
+            Console.WriteLine();
+            Console.WriteLine($"        Строка        | Количество | Частота |");
+
+            foreach (var kvp in frequencies)
+            {
+                Console.WriteLine(
+                    $"{kvp.Key, -21} " +
+                    $"| {CustomString.CreateInstance(kvp.Value.ToString().ToCharArray()).FormatCenter(10)} " +
+                    $"| {CustomString.CreateInstance(((kvp.Value + .0) / wordsCount).ToString("0.####").ToCharArray()).FormatCenter(8)}" +
+                    $"| ");
+            }
+        }
+
+        private static Dictionary<CustomString, int> CalculateStringsFrequencies(CustomString text, bool dependOnRegister, params char[] separators)
+        {
+            if (!dependOnRegister)
+                text = text.ReplaceByDelegate(c => char.ToLower(c));
+
+            Dictionary<CustomString, int> result = new Dictionary<CustomString, int>();
+
+            List<char> currentStringSymbols = new List<char>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (separators.Contains(text[i]))
+                {
+                    CustomString str = CustomString.CreateInstance(currentStringSymbols.ToArray());
+
+                    currentStringSymbols.Clear();
+
+                    AddStringToFrequencies(result, str);
+
+                    continue;
+                }
+
+                currentStringSymbols.Add(text[i]);
+            }
+
+            AddStringToFrequencies(result, CustomString.CreateInstance(currentStringSymbols.ToArray()));
+
+            return result;
+        }
+
+        private static void AddStringToFrequencies(Dictionary<CustomString, int> frequencies, CustomString str)
+        {
+            if (str == CustomString.Empty)
+                return;
+
+            if (frequencies.ContainsKey(str))
+                frequencies[str]++;
+            else
+                frequencies.Add(str, 1);
+        }
+    }
+}
