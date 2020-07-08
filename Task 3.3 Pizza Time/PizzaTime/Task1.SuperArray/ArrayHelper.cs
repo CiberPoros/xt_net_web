1e23bdada49071371e1e3004208722e04a0ffe11
Завенягин Максим <maximzombi@yandex.ru>
2020-07-09 03:44:52 +0400

    Task 3.3.1 : SuperArray : Average method overloads added, GetCommonest method added.

--- Task 3.3 Pizza Time/PizzaTime/Task1.SuperArray/ArrayHelper.cs
+++ Task 3.3 Pizza Time/PizzaTime/Task1.SuperArray/ArrayHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
 
 namespace Task1.SuperArray
 {
@@ -28,6 +30,20 @@ namespace Task1.SuperArray
 
             for (int i = 0; i < array.Length; i++)
                 array[i] = transformer(array[i]);
+        }
+
+        public static T GetCommonest<T>(this T[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array
+                .GroupBy(val => val)
+                .Aggregate(default(IGrouping<T, T>), (commonest, next) => next.Count() > commonest.Count() ? next : commonest)
+                .Key;
         }
 
         #region SUM
@@ -368,6 +384,276 @@ namespace Task1.SuperArray
                 throw new OverflowException("Ariphmetic operation resulted in an overflow.", e);
             }
         }
+        #endregion
+
+        #endregion
+
+        #region AVERAGE
+
+        #region NOT_NULLABLE
+
+        public static int Average(this int[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        public static long Average(this long[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        public static double Average(this double[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        public static decimal Average(this decimal[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        public static float Average(this float[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        #endregion
+
+        #region NULLABLE
+
+        public static int? Average(this int?[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        public static long? Average(this long?[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        public static double? Average(this double?[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        public static decimal? Average(this decimal?[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        public static float? Average(this float?[] array)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average();
+        }
+
+        #endregion
+
+        #region GENERIC_WITH_SELECTOR_TO_NOT_NULLABLE
+
+        public static int Average<T>(this T[] array, Func<T, int> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
+        public static long Average<T>(this T[] array, Func<T, long> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
+        public static double Average<T>(this T[] array, Func<T, double> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
+        public static decimal Average<T>(this T[] array, Func<T, decimal> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
+        public static float Average<T>(this T[] array, Func<T, float> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
+        #endregion
+
+        #region GENERIC_WITH_SELECTOR_TO_NULLABLE
+
+        public static int? Average<T>(this T[] array, Func<T, int?> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
+        public static long? Average<T>(this T[] array, Func<T, long?> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
+        public static double? Average<T>(this T[] array, Func<T, double?> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
+        public static decimal? Average<T>(this T[] array, Func<T, decimal?> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
+        public static float? Average<T>(this T[] array, Func<T, float?> selector)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array), $"Parameter {array} can't be null.");
+
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector), $"Parameter {selector} can't be null.");
+
+            if (!array.Any())
+                throw new ArgumentException(nameof(array), $"Parameter {array} must contain at least one element.");
+
+            return array.Average(selector);
+        }
+
         #endregion
 
         #endregion
