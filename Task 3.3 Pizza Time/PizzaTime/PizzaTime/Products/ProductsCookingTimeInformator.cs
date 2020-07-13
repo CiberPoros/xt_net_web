d4a1c8b207b805d98d45951b02ee445aa2abc117
Завенягин Максим <maximzombi@yandex.ru>
2020-07-13 14:03:22 +0400

    Task 3.3.3 : PizzaTime : locker to ProductsCookingTimeInformator added

--- Task 3.3 Pizza Time/PizzaTime/PizzaTime/Products/ProductsCookingTimeInformator.cs
+++ Task 3.3 Pizza Time/PizzaTime/PizzaTime/Products/ProductsCookingTimeInformator.cs
@@ -5,19 +5,24 @@ namespace PizzaTime.Products
 {
     internal static class ProductsCookingTimeInformator
     {
+        private static readonly object _locker = new object();
+
         private readonly static Dictionary<ProductType, AbstractProduct> _instances = new Dictionary<ProductType, AbstractProduct>();
 
         public static int GetCookingTime(ProductType productType)
         {
-            if (_instances.TryGetValue(productType, out AbstractProduct instance))
+            lock (_locker)
             {
-                return instance.CookingTimeInSeconds;
+                if (_instances.TryGetValue(productType, out AbstractProduct instance))
+                {
+                    return instance.CookingTimeInSeconds;
+                }
+
+                instance = AbstractProductCreator.GetCreator(productType).Create();
+                _instances.Add(productType, instance);
+
+                return instance.CookingTimeInSeconds; 
             }
-
-            instance = AbstractProductCreator.GetCreator(productType).Create();
-            _instances.Add(productType, instance);
-
-            return instance.CookingTimeInSeconds;
         }
     }
 }
