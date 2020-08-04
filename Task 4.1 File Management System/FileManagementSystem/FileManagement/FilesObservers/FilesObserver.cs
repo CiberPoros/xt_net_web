dbfd1f00885db58bab39baf0aab6f969912932f1
Завенягин Максим <maximzombi@yandex.ru>
2020-08-04 15:48:25 +0400

    Task 4.1.1 : FilisManagement : FilesObserver refactored and subdivided into regions

--- Task 4.1 File Management System/FileManagementSystem/FileManagement/FilesObservers/FilesObserver.cs
+++ Task 4.1 File Management System/FileManagementSystem/FileManagement/FilesObservers/FilesObserver.cs
@@ -12,9 +12,10 @@ namespace FileManagement.FilesObservers
     public class FilesObserver : IFilesObserver
     {
         private const string DirectoryDoesNotExistError = "Directory does not exist";
-        private const string FileNotFountError = "Error of load last backup. File not found.";
+        private const string LastBackupNotFountError = "Error of load last backup. File not found.";
         private const string LastBackupLoadError = "Error of load last backup.";
-        private const string ComparingFilesError = "Something went wrong with files comparing...";
+        private const string LoadFileError = "Error of load file.";
+        private const string FileNotFoundError = "Error of load file. File not found.";
 
         private bool _wasChangeOnLastCheck = false;
 
@@ -35,14 +36,16 @@ namespace FileManagement.FilesObservers
             {
                 LoadLastBackup();
             }
-            catch (FileNotFoundException)
+            catch (FileNotFoundException e)
             {
-                Debug.WriteLine(FileNotFountError);
+                Debug.WriteLine(LastBackupNotFountError);
+                Debug.WriteLine(e);
                 Init(path);
             }
-            catch (Exception)
+            catch (IOException e)
             {
                 Debug.WriteLine(LastBackupLoadError);
+                Debug.WriteLine(e);
                 Init(path);
             }
         }
@@ -60,119 +63,30 @@ namespace FileManagement.FilesObservers
             }
         }
 
-        private void Observe(DirectoryObject directoryObject, DateTime dateTime)
+        private static bool TryReadData(FileObject fileObject, out byte[] data)
         {
-            CheckDeleteFileSystemObjects(directoryObject, dateTime);
+            data = null;
+            var fi = new FileInfo(fileObject.FullPath);
 
-            CheckAddFileSystemObjects(directoryObject, dateTime);
+            if (!fi.Exists)
+                return false;
 
-            CheckFilesChanges(directoryObject, dateTime);
-        }
-
-        private void CheckFilesChanges(DirectoryObject directoryObject, DateTime dateTime)
-        {
-            foreach (var innerFileObject in directoryObject.InnerFileObjects)
+            try
             {
-                var fi = new FileInfo(innerFileObject.FullPath);
-
-                if (!fi.Exists)
-                    continue;
-
-                byte[] data;
-
-                try
-                {
-                    data = File.ReadAllBytes(fi.FullName);
-                }
-                catch (Exception)
-                {
-                    Debug.WriteLine(ComparingFilesError);
-                    continue;
-                }
-
-                if (innerFileObject.LastData != null && Utils.GetHash(innerFileObject.LastData) == Utils.GetHash(data))
-                    continue;
-
-                innerFileObject.LastData = data;
-
-                var temp = new byte[data.Length];
-                Array.Copy(data, temp, data.Length);
-
-                innerFileObject.FileChangeDescriptions.Add(new DataChangeFileSystemObjectDescription(dateTime, temp));
-                _wasChangeOnLastCheck = true;
+                data = File.ReadAllBytes(fileObject.FullPath);
+                return true;
             }
-        }
-
-        private void CheckAddFileSystemObjects(DirectoryObject directoryObject, DateTime dateTime)
-        {
-            DirectoryInfo currentDirectoryInfo = new DirectoryInfo(directoryObject.FullPath);
-
-            if (currentDirectoryInfo.Exists)
+            catch (FileNotFoundException e)
             {
-                foreach (var directoryInfo in currentDirectoryInfo.GetDirectories())
-                {
-                    if (directoryObject.InnerDirectoryObjects.All(dirObj => dirObj.FullPath != directoryInfo.FullName))
-                    {
-                        var dirObj = new DirectoryObject(directoryInfo.FullName, dateTime);
-                        directoryObject.InnerDirectoryObjects.Add(dirObj);
-                        dirObj.FileChangeDescriptions.Add(new CreateFileSystemObjectDescription(dateTime));
-                        _wasChangeOnLastCheck = true;
-
-                        Observe(dirObj, dateTime);
-                    }
-                }
+                Debug.WriteLine(FileNotFoundError);
+                Debug.WriteLine(e);
+                return false;
             }
-
-            if (currentDirectoryInfo.Exists)
+            catch (IOException e)
             {
-                foreach (var fileInfo in currentDirectoryInfo.GetFiles())
-                {
-                    if (directoryObject.InnerFileObjects.All(fileObj => fileObj.FullPath != fileInfo.FullName))
-                    {
-                        var fileObject = new FileObject(fileInfo.FullName, dateTime);
-                        directoryObject.InnerFileObjects.Add(fileObject);
-                        fileObject.FileChangeDescriptions.Add(new CreateFileSystemObjectDescription(dateTime));
-                        _wasChangeOnLastCheck = true;
-                    }
-                    else if (directoryObject.InnerFileObjects.Find(fileObj =>
-                        fileObj.FullPath == fileInfo.FullName && fileObj.LastData == null) != null)
-                    {
-                        var obj = directoryObject.InnerFileObjects.Find(fileObj =>
-                            fileObj.FullPath == fileInfo.FullName && fileObj.LastData == null);
-
-                        if (obj != null)
-                            _wasChangeOnLastCheck = true;
-
-                        obj?.FileChangeDescriptions.Add(new CreateFileSystemObjectDescription());
-                    }
-                }
-            }
-        }
-
-        private void CheckDeleteFileSystemObjects(DirectoryObject directoryObject, DateTime dateTime)
-        {
-            foreach (var innerDirectoryObject in directoryObject.InnerDirectoryObjects)
-            {
-                if (innerDirectoryObject.FileChangeDescriptions.Any() && innerDirectoryObject.FileChangeDescriptions.Last() is DeleteFileSystemObjectDescription)
-                    continue;
-
-                if (!Directory.Exists(innerDirectoryObject.FullPath))
-                {
-                    innerDirectoryObject.FileChangeDescriptions.Add(new DeleteFileSystemObjectDescription(dateTime));
-                    _wasChangeOnLastCheck = true;
-                }
-
-                Observe(innerDirectoryObject, dateTime);
-            }
-
-            foreach (var innerFileObject in directoryObject.InnerFileObjects)
-            {
-                if (innerFileObject.LastData != null && !File.Exists(innerFileObject.FullPath))
-                {
-                    innerFileObject.FileChangeDescriptions.Add(new DeleteFileSystemObjectDescription(dateTime));
-                    innerFileObject.LastData = null;
-                    _wasChangeOnLastCheck = true;
-                }
+                Debug.WriteLine(LoadFileError);
+                Debug.WriteLine(e);
+                return false;
             }
         }
 
@@ -183,6 +97,128 @@ namespace FileManagement.FilesObservers
 
             Save();
         }
+
+        private void Observe(DirectoryObject directoryObject, DateTime dateTime)
+        {
+            CheckDeleteFileSystemObjects(directoryObject, dateTime);
+
+            CheckAddFileSystemObjects(directoryObject, dateTime);
+
+            CheckFilesChanges(directoryObject, dateTime);
+        }
+
+        #region CHECK_INNER_FILES_CHANGES
+        private void CheckFilesChanges(DirectoryObject directoryObject, DateTime dateTime)
+        {
+            foreach (var innerFileObject in directoryObject.InnerFileObjects)
+                CheckFileChange(innerFileObject, dateTime);
+        }
+
+        private void CheckFileChange(FileObject fileObject, DateTime dateTime)
+        {
+            if (!TryReadData(fileObject, out var data))
+                return;
+
+            if (fileObject.LastData != null && Utils.GetHash(fileObject.LastData) == Utils.GetHash(data))
+                return;
+
+            fileObject.LastData = data;
+
+            var temp = new byte[data.Length];
+            Array.Copy(data, temp, data.Length);
+
+            fileObject.FileChangeDescriptions.Add(new DataChangeFileSystemObjectDescription(dateTime, temp));
+            _wasChangeOnLastCheck = true;
+        } 
+        #endregion
+
+        #region CHECK_ADD_TO_DIRECTORY_OBJECT_AND_INNER_OBJECTS
+        private void CheckAddFileSystemObjects(DirectoryObject directoryObject, DateTime dateTime)
+        {
+            DirectoryInfo currentDirectoryInfo = new DirectoryInfo(directoryObject.FullPath);
+
+            if (!currentDirectoryInfo.Exists)
+                return;
+
+            foreach (var directoryInfo in currentDirectoryInfo.GetDirectories())
+                CheckAddDirectory(directoryObject, directoryInfo, dateTime);
+
+            if (!currentDirectoryInfo.Exists)
+                return;
+
+            foreach (var fileInfo in currentDirectoryInfo.GetFiles())
+                CheckAddFile(directoryObject, fileInfo, dateTime);
+        }
+
+        private void CheckAddDirectory(DirectoryObject directoryObject, DirectoryInfo innerDirectoryInfo, DateTime dateTime)
+        {
+            if (directoryObject.InnerDirectoryObjects.Any(dirObj => dirObj.FullPath == innerDirectoryInfo.FullName))
+                return;
+
+            var dirObject = new DirectoryObject(innerDirectoryInfo.FullName, dateTime);
+            directoryObject.InnerDirectoryObjects.Add(dirObject);
+            dirObject.FileChangeDescriptions.Add(new CreateFileSystemObjectDescription(dateTime));
+            _wasChangeOnLastCheck = true;
+
+            Observe(dirObject, dateTime);
+        }
+
+        private void CheckAddFile(DirectoryObject directoryObject, FileInfo innerFileInfo, DateTime dateTime)
+        {
+            if (directoryObject.InnerFileObjects.All(fileObj => fileObj.FullPath != innerFileInfo.FullName))
+            {
+                var fileObject = new FileObject(innerFileInfo.FullName, dateTime);
+                directoryObject.InnerFileObjects.Add(fileObject);
+                fileObject.FileChangeDescriptions.Add(new CreateFileSystemObjectDescription(dateTime));
+                _wasChangeOnLastCheck = true;
+                return;
+            }
+
+            var deletedFileObject = directoryObject.InnerFileObjects.Find(fileObj =>
+                fileObj.FullPath == innerFileInfo.FullName && fileObj.LastData == null);
+
+            if (deletedFileObject == null)
+                return;
+
+            _wasChangeOnLastCheck = true;
+            deletedFileObject.FileChangeDescriptions.Add(new CreateFileSystemObjectDescription());
+        }
+        #endregion
+
+        #region CHECK_DELETE_FYLE_SYSTEM_OBJECTS_AND_INNER_OBJECTS
+        private void CheckDeleteFileSystemObjects(DirectoryObject directoryObject, DateTime dateTime)
+        {
+            foreach (var innerDirectoryObject in directoryObject.InnerDirectoryObjects)
+                CheckDeleteDirectory(innerDirectoryObject, dateTime);
+
+            foreach (var innerFileObject in directoryObject.InnerFileObjects)
+                CheckDeleteFile(innerFileObject, dateTime);
+        }
+
+        private void CheckDeleteDirectory(DirectoryObject directoryObject, DateTime dateTime)
+        {
+            if (directoryObject.FileChangeDescriptions.Any() && directoryObject.FileChangeDescriptions.Last() is DeleteFileSystemObjectDescription)
+                return;
+
+            if (!Directory.Exists(directoryObject.FullPath))
+            {
+                directoryObject.FileChangeDescriptions.Add(new DeleteFileSystemObjectDescription(dateTime));
+                _wasChangeOnLastCheck = true;
+            }
+
+            Observe(directoryObject, dateTime);
+        }
+
+        private void CheckDeleteFile(FileObject fileObject, DateTime dateTime)
+        {
+            if (fileObject.LastData == null || File.Exists(fileObject.FullPath))
+                return;
+
+            fileObject.FileChangeDescriptions.Add(new DeleteFileSystemObjectDescription(dateTime));
+            fileObject.LastData = null;
+            _wasChangeOnLastCheck = true;
+        } 
+        #endregion
 
         private void Save()
         {
