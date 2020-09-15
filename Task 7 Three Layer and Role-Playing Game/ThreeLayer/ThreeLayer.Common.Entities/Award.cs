be2d5d78259b82046ddd6e3e3f16c9150f78d52d
Завенягин Максим <maximzombi@yandex.ru>
2020-09-15 10:52:05 +0400

    Task7.1 : Three Layer BLL : base entities added

---
+++ Task 7 Three Layer and Role-Playing Game/ThreeLayer/ThreeLayer.Common.Entities/Award.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThreeLayer.Common.Entities
+{
+    public class Award
+    {
+        private string _title;
+        private IEnumerable<User> _owners;
+
+        public int Id { get; set; }
+
+        public string Title 
+        { 
+            get => _title; 
+            set => _title = string.IsNullOrWhiteSpace(value) ? throw new ArgumentNullException(nameof(value), "String can't be null or white space.") : value; 
+        }
+
+        public IEnumerable<User> Owners
+        {
+            get => _owners;
+            set => _owners = value ?? throw new ArgumentNullException(nameof(value), "Argument is null.");
+        }
+    }
+}
---
+++ Task 7 Three Layer and Role-Playing Game/ThreeLayer/ThreeLayer.Common.Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThreeLayer.Common.Entities
+{
+    public class User
+    {
+        private int _age;
+        private IEnumerable<Award> _awards;
+
+        public User(int id, string name, DateTime dateOfBirth, int age)
+        {
+            Id = id;
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            Age = age;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value >= 0 ? value : throw new ArgumentOutOfRangeException(nameof(value), value, "Age can't be negative.");
+        }
+
+        public IEnumerable<Award> Awards 
+        { 
+            get => _awards; 
+            set => _awards = value ?? throw new ArgumentNullException(nameof(value), "Argument is null."); 
+        }
+    }
+}
