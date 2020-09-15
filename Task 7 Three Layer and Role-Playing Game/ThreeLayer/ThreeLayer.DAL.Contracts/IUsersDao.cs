d42984d1d1ba744b824b4e07f46bda1f879aea42
Завенягин Максим <maximzombi@yandex.ru>
2020-09-15 12:10:42 +0400

    Task7.1 : Three Layer DAL : contracts added

---
+++ Task 7 Three Layer and Role-Playing Game/ThreeLayer/ThreeLayer.DAL.Contracts/IUsersDao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreeLayer.Common.Entities;
+
+namespace ThreeLayer.DAL.Contracts
+{
+    public interface IUsersDao
+    {
+        void AddUser(User user);
+        void RemoveUserById(int id);
+        IEnumerable<User> GetAllUsers();
+
+        void AddAwardToUser(User user, Award award);
+        void RemoveAwardFromUser(User user, Award award);
+    }
+}
