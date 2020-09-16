15d7837e222db4064b72416cab9c8104f9b81db7
Завенягин Максим <maximzombi@yandex.ru>
2020-09-16 13:21:11 +0400

    Task 7.1 : Three Layer PL : base methods added

---
+++ Task 7 Three Layer and Role-Playing Game/ThreeLayer/ThreeLayer.PL.Console/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeLayer.PL.Console
+{
+    class Program
+    {
+        private static readonly Dictionary<UIActionType, string> _discriprionsOfActions = new Dictionary<UIActionType, string>
+        {
+            { UIActionType.ADD_USER, "Добавить пользователя;" },
+            { UIActionType.GET_ALL_USERS, "Показать всех пользователей;" },
+            { UIActionType.REMOVE_USER_BY_ID, "Удалить пользователя по ID;" },
+            { UIActionType.ADD_AWARD, "Добавить награду;" },
+            { UIActionType.GET_ALL_AWARDS, "Показать все возможные награды;" },
+            { UIActionType.REMOVE_AWARD_BY_ID, "Удалить награду по ID;" },
+            { UIActionType.ADD_AWARD_TO_USER, "Добавить награду для пользователя;" },
+            { UIActionType.REMOVE_AWARD_FROM_USER, "Удалить награду у пользователя..." }
+        };
+
+        static void Main(string[] args)
+        {
+            OutActionTypes();
+            var at = ReadActionType();
+            System.Console.WriteLine(at);
+        }
+
+        public static void OutActionTypes()
+        {
+            foreach (var kvp in _discriprionsOfActions)
+            {
+                System.Console.WriteLine($"{(int)kvp.Key} - {kvp.Value}");
+            }
+        }
+
+        private static UIActionType ReadActionType()
+        {
+            var res = UIActionType.NONE;
+
+            while (res == UIActionType.NONE)
+            {
+                var input = System.Console.ReadKey().Key;
+                var keyValue = input >= ConsoleKey.D1 && input <= ConsoleKey.D8 ? input - ConsoleKey.D0 : 0;
+                if (keyValue == 0)
+                    keyValue = input >= ConsoleKey.NumPad1 && input <= ConsoleKey.NumPad8 ? input - ConsoleKey.NumPad0 : 0;
+
+                res = (UIActionType)keyValue;
+
+                if (res == UIActionType.NONE)
+                    ClearLastSymbolInConsole();
+            }
+
+            return res;
+        }
+
+        private static void ClearLastSymbolInConsole()
+        {
+            System.Console.SetCursorPosition(System.Console.CursorLeft - 1, System.Console.CursorTop);
+            System.Console.Write(" ");
+            System.Console.SetCursorPosition(System.Console.CursorLeft - 1, System.Console.CursorTop);
+        }
+    }
+}
---
+++ Task 7 Three Layer and Role-Playing Game/ThreeLayer/ThreeLayer.PL.Console/UIActionType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeLayer.PL.Console
+{
+    public enum UIActionType
+    {
+        NONE = 0,
+        ADD_USER = 1,
+        GET_ALL_USERS = 2,
+        REMOVE_USER_BY_ID = 3,
+        ADD_AWARD = 4,
+        GET_ALL_AWARDS = 5,
+        REMOVE_AWARD_BY_ID = 6,
+        ADD_AWARD_TO_USER = 7,
+        REMOVE_AWARD_FROM_USER = 8
+    }
+}
