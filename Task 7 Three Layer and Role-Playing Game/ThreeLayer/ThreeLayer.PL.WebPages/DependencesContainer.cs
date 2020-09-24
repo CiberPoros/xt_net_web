ad5db0bbcca93e865722b0a2038ab5ebd69badaf
Завенягин Максим <maximzombi@yandex.ru>
2020-09-24 19:43:09 +0400

    Task 7.1 : ThreeLayer : simple web UI added

---
+++ Task 7 Three Layer and Role-Playing Game/ThreeLayer/ThreeLayer.PL.WebPages/DependencesContainer.cs
@@ -0,0 +1,15 @@
+﻿using Ninject;
+using ThreeLayer.Common.Dependences;
+
+namespace ThreeLayer.PL.WebPages
+{
+    public static class DependencesContainer
+    {
+        public static readonly IKernel Resolver;
+
+        static DependencesContainer()
+        {
+            Resolver = new StandardKernel(new NinjectRegistrationsDAL(), new NinjectRegistrationsBLL());
+        }
+    }
+}
